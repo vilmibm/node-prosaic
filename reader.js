a44bccef66cb2d37504ac11dbe3d95b30997580f
Nate Smith <nate.smith@coxinc.com>
2011-08-21 21:44:37 -0400

    implement a phrase tokenizer

--- reader.js
+++ reader.js
@@ -1,10 +1,26 @@
 #!/usr/bin/env node
 // given a directory
 // process every file in the directory
-// store a line in mongo-- {raw, source_filename, metadata}
+// store a phrase in mongo-- {raw, source_filename, metadata}
 
 // on file
 // on directory
+
+// pos thoughts
+/*
+
+want: smallest unit of phrase that could stand alone in a poem
+
+The brown dog smiles, not knowing whether the cat is delicious
+The bag is full, necessarily, of walnuts
+
+I used to live in the U.S.S.R. but now I live in "Amerika." Entirely wugget, she thought.
+
+stream, add to buffer.
+
+CHAR a-zA-Z, '()
+
+*/
 
 var events = require('events');
 var fs = require('fs');
@@ -17,8 +33,61 @@ var mongo_db = 'noetry_dev';
 var mongo_server = '127.0.0.1';
 var mongos = new mongodb.Server(mongo_server, mongo_port, {});
 
+function Tokenizer() {
+    this._buffer = '';
+    events.EventEmitter.call(this);
+}
+sys.inherits(Tokenizer, events.EventEmitter);
+Tokenizer.prototype.write = function(data) {
+  for (c in data) {
+    this._buffer += data[c];
+    if (data[c].match(/[.,;:]/)) {
+      this.emit('phrase', this._buffer);
+      this._buffer = '';
+    }
+  }
+  this.emit('end');
+};
+
+
 function FileCrawler() {
   events.EventEmitter.call(this);
+  this.on('dir', function(dir) { this.crawl(dir); });
+
+  this.on('file', function(file) {
+    files_seen++;
+    console.log('file ' + file);
+    var self = this;
+    // TODO this might hurt but makes coding so much easier
+    // TODO assumes utf
+    fs.readFile(file, 'utf8', function(err, data) {
+      // TODO strings are probably not efficient and buffers may help. start naive.
+      // TODO feed whole file to a tokenizer
+      var t = new Tokenizer();
+      t.on('phrase', function(match) {
+        phrases_seen++;
+        var phrases = new mongodb.Collection(self.client, 'phrases');
+        console.log(match);
+        phrases.insert({
+          'raw': match,
+          'source': file
+        }, function() {
+          phrases_done++;
+          if (files_done == files_seen && phrases_done == phrases_seen) {
+            self.client.close();
+          }
+        });
+      });
+      t.on('end', function() {
+        files_done++;
+        console.log(files_seen, files_done, phrases_done, phrases_seen);
+        if (files_done == files_seen && phrases_done == phrases_seen) {
+          self.client.close();
+        }
+      });
+      t.write(data);
+    });
+  });
 }
 
 sys.inherits(FileCrawler, events.EventEmitter);
@@ -36,46 +105,13 @@ FileCrawler.prototype.crawl = function(path) {
   });
 };
 
+var phrases_seen = 0;
+var phrases_done = 0;
+var files_seen = 0;
+var files_done = 0;
 new mongodb.Db(mongo_db, mongos, {}).open(function(err, client) {
-  var lines_seen = 0;
-  var lines_done = 0;
-  var files_seen = 0;
-  var files_done = 0;
   var fc = new FileCrawler();
-  fc.on('dir', function(dir) { this.crawl(dir); });
-
-  fc.on('file', function(file) {
-    files_seen++;
-    console.log('file ' + file);
-    var self = this;
-    // TODO this might hurt but makes coding so much easier
-    // TODO assumes utf
-    fs.readFile(file, 'utf8', function(err, data) {
-      // TODO strings are probably not efficient and buffers may help. start naive.
-      // TODO feed whole file to a tokenizer
-      data.split('.').forEach(function(x) {
-        lines_seen++;
-        self.emit('line', file, x);
-      });
-      files_done++;
-      if (files_seen == files_done) {
-        if (lines_seen == lines_done) {
-          client.close();
-        }
-      }
-    });
-  });
-
-  fc.on('line', function(file, line) {
-    var lines = new mongodb.Collection(client, 'lines');
-    // TODO collect metadata
-    lines.insert({
-      'raw': line,
-      'source': file
-    });
-    lines_done++;
-  });
-
+  fc.client = client;
   // TODO fix this, make all relative
   process.chdir('samples');
 
