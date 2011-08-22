9d7a3375f0b0fbc2a542b20331115e6c22926cf6
Nate Smith <nate.smith@coxinc.com>
2011-08-21 22:17:08 -0400

    clean up and fix \r\n scrapper

--- reader.js
+++ reader.js
@@ -3,25 +3,6 @@
 // process every file in the directory
 // store a phrase in mongo-- {raw, source_filename, metadata}
 
-// on file
-// on directory
-
-// pos thoughts
-/*
-
-want: smallest unit of phrase that could stand alone in a poem
-
-The brown dog smiles, not knowing whether the cat is delicious
-The bag is full, necessarily, of walnuts
-
-I used to live in the U.S.S.R. but now I live in "Amerika." Entirely wugget, she thought.
-
-stream, add to buffer.
-
-CHAR a-zA-Z, '()
-
-*/
-
 var events = require('events');
 var fs = require('fs');
 var sys = require('sys');
@@ -34,16 +15,22 @@ var mongo_server = '127.0.0.1';
 var mongos = new mongodb.Server(mongo_server, mongo_port, {});
 
 function Tokenizer() {
-    this._buffer = '';
-    events.EventEmitter.call(this);
+  this._buffer = '';
+  events.EventEmitter.call(this);
 }
 sys.inherits(Tokenizer, events.EventEmitter);
 Tokenizer.prototype.write = function(data) {
   for (c in data) {
-    this._buffer += data[c];
-    if (data[c].match(/[.,;:]/)) {
+    if (data[c] === "\n" || data[c] === "\r") {
+      this._buffer += ' ';
+    }
+    else if (data[c].match(/[.,;:]/)) {
+      this._buffer += data[c];
       this.emit('phrase', this._buffer);
       this._buffer = '';
+    }
+    else {
+      this._buffer += data[c];
     }
   }
   this.emit('end');
@@ -52,12 +39,15 @@ Tokenizer.prototype.write = function(data) {
 
 function FileCrawler() {
   events.EventEmitter.call(this);
+  this.phrases_seen = 0;
+  this.phrases_done = 0;
+  this.files_seen = 0;
+  this.files_done = 0;
   this.on('dir', function(dir) { this.crawl(dir); });
 
   this.on('file', function(file) {
-    files_seen++;
-    console.log('file ' + file);
     var self = this;
+    self.files_seen++;
     // TODO this might hurt but makes coding so much easier
     // TODO assumes utf
     fs.readFile(file, 'utf8', function(err, data) {
@@ -65,23 +55,21 @@ function FileCrawler() {
       // TODO feed whole file to a tokenizer
       var t = new Tokenizer();
       t.on('phrase', function(match) {
-        phrases_seen++;
+        self.phrases_seen++;
         var phrases = new mongodb.Collection(self.client, 'phrases');
-        console.log(match);
         phrases.insert({
           'raw': match,
           'source': file
         }, function() {
-          phrases_done++;
-          if (files_done == files_seen && phrases_done == phrases_seen) {
+          self.phrases_done++;
+          if (self.done()) {
             self.client.close();
           }
         });
       });
       t.on('end', function() {
-        files_done++;
-        console.log(files_seen, files_done, phrases_done, phrases_seen);
-        if (files_done == files_seen && phrases_done == phrases_seen) {
+        self.files_done++;
+        if (self.done()) {
           self.client.close();
         }
       });
@@ -105,10 +93,10 @@ FileCrawler.prototype.crawl = function(path) {
   });
 };
 
-var phrases_seen = 0;
-var phrases_done = 0;
-var files_seen = 0;
-var files_done = 0;
+FileCrawler.prototype.done = function() {
+  return this.files_seen === this.files_done && this.phrases_seen === this.phrases_done;
+}
+
 new mongodb.Db(mongo_db, mongos, {}).open(function(err, client) {
   var fc = new FileCrawler();
   fc.client = client;
